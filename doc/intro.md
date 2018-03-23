07f2f6a354b37207ac05d840a1963a9d1f57fa2b
Sascha Koch <sascha.koch@web.de>
2018-03-23 19:26:41 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-mandelbrot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_mandelbrot/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-mandelbrot.core-test
+  (:require [clojure.test :refer :all]
+            [clj-mandelbrot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
